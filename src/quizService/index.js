95fc8d46beefb5b28f6fad90bf4b95cccf465690
David <dcomtech16@gmail.com>
2022-04-01 23:14:47 +0100

    first react app

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "What is the capital of Nigeria?   ",
+    answers: ["Abuja", "Abia", "lagos", "Ogbomosho"],
+    correct: "Abuja",
+    questionId: "099099"
+  },
+  {
+    question:
+      'Sqi is a college of?"?',
+    answers: ["Food", "Music", "Ict", "Dance"],
+    correct: "Ict",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Who is the best react teacher?",
+    answers: ["Mr Seyi", "Director", "Google", "Mr David"],
+    correct: "Google",
+    questionId: "267908"
+  },
+  {
+    question: "What's the name of Batman's  parents?",
+    answers: [
+      "Thomas & Martha",
+      "Joey & Jackie",
+      "Jason & Sarah",
+      "Todd & Mira"
+    ],
+    correct: "Thomas & Martha",
+    questionId: "333247"
+  },
+  {
+    question: "Facebook was built using?",
+    answers: ["React", "Java", "C++", "Ruby"],
+    correct: "React",
+    questionId: "496293"
+  },
+  {
+    question:
+      "Which of these is not a brand of phone?",
+    answers: [
+      "Tecno",
+      "Iphone",
+      "Nokia ",
+      "Mech"
+    ],
+    correct: "Mek",
+    questionId: "588909"
+  },
+  {
+    question:
+      'What name represents the letter "M" in the NATO phonetic alphabet?',
+    answers: ["Mike", "Matthew", "Mark", "Max"],
+    correct: "Mike",
+    questionId: "648452"
+  },
+  {
+    question: "What is the first book of the Old Testament?",
+    answers: ["Genesis", "Exodus", "Leviticus", "Numbers"],
+    correct: "Genesis",
+    questionId: "786649"
+  },
+  {
+    question:
+      "React uses JSX?",
+    answers: ["True", "False", "Maybe", "I dunno"],
+    correct: "Yes",
+    questionId: "839754"
+  },
+  {
+    question:
+      "NIgeria is in which continent?",
+    answers: [
+      "Africa",
+      "Asia",
+      "Europe",
+      "Australia"
+    ],
+    correct: "Africa",
+    questionId: "98390"
+  },
+  {
+    question: "Who is the president of NIgeria?",
+    answers: ["Buhari", "Mr seyi", "Lukmon", "Saraki"],
+    correct: "Buhari",
+    questionId: "1071006"
+  },
+  {
+    question: "What is mad in yoruba?",
+    answers: ["Werey", "Ode", "Mumu", "jkaxs"],
+    correct: "Werey",
+    questionId: "1174154"
+  },
+  {
+    question: "Area 51 is located in which US state?",
+    answers: ["Nevada", "Arizona", "New Mexico", "Utah"],
+    correct: "Nevada",
+    questionId: "1226535"
+  },
+  {
+    question: "How would one say goodbye in Spanish?",
+    answers: ["Adiós", " Hola", "Au Revoir", "Salir"],
+    correct: "Adiós",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the largest organ of the human body?",
+    answers: ["Skin", "Heart", "large Intestine", "Liver"],
+    correct: "Skin",
+    questionId: "1436873"
+  },
+  {
+    question: "Which sign of the zodiac is represented by the Crab?",
+    answers: ["Cancer", "Libra", "Virgo", "Sagittarius"],
+    correct: "Cancer",
+    questionId: "1515110"
+  },
+  {
+    question: "On a dartboard, what number is directly opposite No. 1?",
+    answers: ["19", "20", "12", "15"],
+    correct: "19",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "What does the 'S' stand for in the abbreviation SIM, as in SIM card? ",
+    answers: ["Subscriber", "Single", "Secure", "Solid"],
+    correct: "Subscriber",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "Which number is directly opposite 1 on the wall clock?",
+    answers: ["1", "12", "7", "8"],
+    correct: "7",
+    questionId: "1822532"
+  },
+  {
+    question: "Which American president appears on a one dollar bill?",
+    answers: [
+      "George Washington",
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "Benjamin Franklin"
+    ],
+    correct: "George Washington",
+    questionId: "195075"
+  },
+  {
+    question: 'What is "dabbing"?',
+    answers: ["A dance", "A medical procedure", "A sport", "A language"],
+    correct: "A dance",
+    questionId: "2019778"
+  },
+  {
+    question: "What is the name of the Biggest State by landmass in Nigeria?",
+    answers: ["Lagos", "Benin", "Abia", "Niger"],
+    correct: "Niger",
+    questionId: "2134343"
+  },
+  {
+    question: "23 * 11 = ?",
+    answers: [
+      "253",
+      "233",
+      "232",
+      "332"
+    ],
+    correct: "253",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "90/5 = ?",
+    answers: ["18", "23", "86", "21"],
+    correct: "18",
+    questionId: "2379831"
+  },
+  {
+    question: "Which animal is known as the king of the jungle?",
+    answers: ["Lizard", "Lion", "Tiger", "Elephant"],
+    correct: "Lion",
+    questionId: "2426418"
+  },
+  {
+    question: "121 + 231?",
+    answers: ["412", "311", "352", "321"],
+    correct: "352",
+    questionId: "2510086"
+  },
+  {
+    question: "What color is on the Nigerian Flag ?",
+    answers: [
+      "Blue",
+      "Grey",
+      "Vanilla",
+      "White"
+    ],
+    correct: "White",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Which artist is the best in Nigeria?",
+    answers: ["Davido", "Wizkid", "Burna boy", "Rhema"],
+    correct: "Rhema",
+    questionId: "2796884"
+  },
+  {
+    question: "Which of these colours is NOT featured in the logo for Google?",
+    answers: ["Pink", "Yellow", "Blue", "Green"],
+    correct: "Pink",
+    questionId: "2838900"
+  },
+  {
+    question: 'What is the French word for "hat"?',
+    answers: ["Chapeau", "Bonnet", " Écharpe", " Casque"],
+    correct: "Chapeau",
+    questionId: "298060"
+  },
+  {
+    question: "Which is greater 100 dollars or a 100 pounds?",
+    answers: ["100 dollar", "100 pounds", "100 naira", "none"],
+    correct: "100 pounds",
+    questionId: "3096579"
+  },
+  {
+    question: "What is chrome known for?",
+    answers: [
+      "Speed",
+      "Accuracy",
+      "Eating ram",
+      "History"
+    ],
+    correct: "Eating ram",
+    questionId: "3182461"
+  },
+  {
+    question: "What do the letters in the GMT time zone stand for?",
+    answers: [
+      "Greenwich Mean Time",
+      "Global Meridian Time",
+      "General Median Time",
+      "Glasgow Man Time"
+    ],
+    correct: "Greenwich Mean Time",
+    questionId: "3239112"
+  },
+  {
+    question: "Which one of these is not a typical Nigerian dish?",
+    answers: ["amala", "Rice", "Hot dog", "Fufu"],
+    correct: "Hot dog",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'As a student which of these is the most important',
+    answers: ["Garri", "Rice", "Beans", "Spag"],
+    correct: "Garri",
+    questionId: "3410327"
+  },
+  {
+    question: "What is the name of the vice president of NIgeria?",
+    answers: ["David", "Saraki", "Tinubu", "Osibanjo"],
+    correct: "Osinbanjo",
+    questionId: "3542596"
+  },
+  {
+    question: "The Vp of Nigeria is a pastor in which church”",
+    answers: ["Rccg", "Mfm", "Winners", "Deeper life"],
+    correct: "Rccg",
+    questionId: "3667517"
+  },
+  {
+    question: "What do the letters of the fast food chain KFC stand for?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: "Which restaurant's mascot is a clown?",
+    answers: ["McDonald's", "Whataburger", "Burger King", "Sonic"],
+    correct: "McDonald's",
+    questionId: "3893585"
+  },
+  {
+    question: 'What color is the "Ex" in FedEx Ground?',
+    answers: ["Green", "Red", "Light Blue", "Orange"],
+    correct: "Green",
+    questionId: "3913430"
+  },
+  {
+    question: "How tall is the Burj Khalifa?",
+    answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
+    correct: "2,722 ft",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Which of the following card games revolves around numbers and basic math?",
+    answers: ["Uno", "Go Fish", "Twister", "Munchkin"],
+    correct: "Uno",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "What machine element is located in the center of fidget spinners?",
+    answers: ["Bearings", "Axles", "Gears", "Belts"],
+    correct: "Bearings",
+    questionId: "4235063"
+  },
+  {
+    question: "Which sign of the zodiac comes between Virgo and Scorpio?",
+    answers: ["Libra", "Gemini", "Taurus", "Capricorn"],
+    correct: "Libra",
+    questionId: "4321002"
+  },
+  {
+    question: "Which of the following presidents is not on Mount Rushmore?",
+    answers: [
+      "John F. Kennedy",
+      "Theodore Roosevelt",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "John F. Kennedy",
+    questionId: "4442286"
+  },
+  {
+    question: "What is Tasmania?",
+    answers: [
+      "An Australian State",
+      "A flavor of Ben and Jerry's ice-cream",
+      "A Psychological Disorder",
+      "The Name of a Warner Brothers Cartoon Character"
+    ],
+    correct: "An Australian State",
+    questionId: "4564430"
+  },
+  {
+    question: "What company developed the vocaloid Hatsune Miku?",
+    answers: ["Crypton Future Media", "Sega", "Sony", "Yamaha Corporation"],
+    correct: "Crypton Future Media",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "Which country, not including Japan, has the most people of japanese decent?",
+    answers: ["Brazil", "China", "South Korea", "United States of America"],
+    correct: "Brazil",
+    questionId: "4795960"
+  },
+  {
+    question: "Which candy is NOT made by Mars?",
+    answers: ["Almond Joy", "M&M's", "Twix", "Snickers"],
+    correct: "Almond Joy",
+    questionId: "4811162"
+  },
+  {
+    question: "In which fast food chain can you order a Jamocha Shake?",
+    answers: ["Arby's", "McDonald's", "Burger King", "Wendy's"],
+    correct: "Arby's",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
